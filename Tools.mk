00ef1683ca7970953da0db786f1a6dcd08f28a77
Cameron Wong <cam@camdar.io>
2023-08-02 12:46:15 -0400

    minor stuff
    Signed-off-by: Cameron Wong <cam@camdar.io>

--- Tools.mk
+++ Tools.mk
@@ -4,6 +4,10 @@
 LYN_DIR := $(BIN_DIR)/lyn
 LYN := $(LYN_DIR)/lyn$(EXE)
 
+COLORZCORE_DIR := $(BIN_DIR)/tools_cache
+$(shell mkdir -p $(COLORZCORE_DIR) > /dev/null)
+COLORZCORE := $(COLORZCORE_DIR)/ColorzCore$(EXE)
+
 $(LYN_DIR)/Makefile:
 	cd bin/lyn && cmake .
 
