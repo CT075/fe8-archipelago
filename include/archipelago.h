a93d1b0262ae16d771d41118cbcb0d37894f35a1
Cameron Wong <cam@camdar.io>
2023-09-14 23:51:19 -0400

    correctly allocate Checks instead of Checks*
    Signed-off-by: Cameron Wong <cam@camdar.io>

--- include/archipelago.h
+++ include/archipelago.h
@@ -12,7 +12,7 @@
 #define sizeof_round(ty) ((sizeof(ty) + sizeof(int)-1) & ~(sizeof(int)-1))
 
 // CR cam: it would be nice to not need to do this address bumping manually
-#define CHECKED_LOCATIONS_ADDR (FREE_RAM_START + sizeof_round(struct ProgressiveCaps))
+#define CHECKED_LOCATIONS_ADDR (PROG_CAPS_ADDR + sizeof_round(struct ProgressiveCaps))
 #define checkedLocations ((struct Checks *)(CHECKED_LOCATIONS_ADDR))
 
 struct APReceivedItem {
@@ -20,7 +20,7 @@ struct APReceivedItem {
   u8 filled;
 };
 
-#define RECEIVED_AP_ITEM_ADDR (CHECKED_LOCATIONS_ADDR + sizeof_round(struct Checks *))
+#define RECEIVED_AP_ITEM_ADDR (CHECKED_LOCATIONS_ADDR + sizeof_round(struct Checks))
 #define apReceivedItem ((struct APReceivedItem *)(RECEIVED_AP_ITEM_ADDR))
 
 void handleChapterClear(ProcPtr parent, int chapterNum);
